880d897d6ad6abcd8f581f603f312155839419ca
thDeng <entireboy@daum.net>
2018-06-20 15:17:33 +0900

    Add RabbitMQ publisher confirm samples

--- rabbitmq/src/main/java/kr/leocat/test/sample/rabbitmq/publisherconfirm/spring/RabbitMqConfig.java
+++ rabbitmq/src/main/java/kr/leocat/test/sample/rabbitmq/publisherconfirm/spring/RabbitMqConfig.java
@@ -3,6 +3,7 @@ package kr.leocat.test.sample.rabbitmq.publisherconfirm.spring;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,6 +19,19 @@ public class RabbitMqConfig {
         // publisher confirm ON
         connectionFactory.setPublisherConfirms(true);
         return connectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory rabbitConnectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(rabbitConnectionFactory);
+        template.setConfirmCallback((correlationData, ack, cause) -> {
+            if(ack) {
+                System.out.println("ACK");
+            } else {
+                System.out.println("NACK: " + cause);
+            }
+        });
+        return template;
     }
 
     @Bean
--- rabbitmq/src/main/java/kr/leocat/test/sample/rabbitmq/publisherconfirm/spring/SimplePublisher.java
+++ rabbitmq/src/main/java/kr/leocat/test/sample/rabbitmq/publisherconfirm/spring/SimplePublisher.java
@@ -5,8 +5,6 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-
 @Component
 public class SimplePublisher {
 
@@ -15,18 +13,6 @@ public class SimplePublisher {
 
     @Autowired
     private Queue q1;
-
-    @PostConstruct
-    private void configPublisherConfirm() {
-        // publisher confirm callback
-        template.setConfirmCallback((correlationData, ack, cause) -> {
-            if(ack) {
-                System.out.println("ACK");
-            } else {
-                System.out.println("NACK: " + cause);
-            }
-        });
-    }
 
     public void send(String message) {
         template.convertAndSend(q1.getName(), message);
